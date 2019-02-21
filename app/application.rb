9fdd7ad62ce49e018cf279f9c8e58f3d60383842
Victoria B <viktorija.bourassa@gmail.com>
2019-02-21 12:25:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,8 @@ class Application
     else !@@items.include?(item)
       resp.write "Item not found"
       resp.status = 400
-    # else
+    else
+        resp.write 'Route not found'
     #   resp.status = 404
     end
   end
