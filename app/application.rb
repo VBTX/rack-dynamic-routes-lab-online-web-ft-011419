e68559c37f343ca822c1f248b7fcdf6af243a33a
Victoria B <viktorija.bourassa@gmail.com>
2019-02-21 12:30:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,10 +8,10 @@ class Application
 
     @@items.each do |item|
 
-    if req.path.match(/items/item)
+    if req.path.match("/items/item")
       resp.write item.price
     end
-  elsif !(@@items.include?(item))
+   elsif !(@@items.include?(item))
       resp.write "Item not found"
       resp.status = 400
     else
