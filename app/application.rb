9fe610ff86ed21a2f1a990973077fafc18003203
Victoria B <viktorija.bourassa@gmail.com>
2019-02-21 12:29:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     if req.path.match(/items/item)
       resp.write item.price
     end
-  elsif !@@items.include?(item)
+  elsif !(@@items.include?(item))
       resp.write "Item not found"
       resp.status = 400
     else
